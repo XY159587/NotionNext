f7e61fc90b423820f58acad42bda6d2ae8710c3c
XY159587 <111336691+XY159587@users.noreply.github.com>
2026-02-14 22:43:02 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -85,7 +85,7 @@ const CONFIG = {
     {
       title_1: 'Swift',
       img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
+      color_1: '#eb6840'，
       title_2: 'Principle',
       img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
       color_2: '#8f55ba'
@@ -99,7 +99,7 @@ const CONFIG = {
       color_2: '#2c51db'
     },
     {
-      title_1: 'JS'，
+      title_1: 'JS',
       img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
       color_1: '#f7cb4f',
       title_2: 'HTML',
