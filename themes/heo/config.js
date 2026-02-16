51e8f75201a448b4d4a62add6ce768d977ac937f
XY159587 <111336691+XY159587@users.noreply.github.com>
2026-02-16 20:59:18 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,8 +51,8 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
+  HEO_INFO_CARD_URL2: 'https://https://www.douyin.com/user/MS4wLjABAAAARjPntiO1ae96zNs7xC9ga0Y-kUJFiVh7MqIsmpco5jQ?from_tab_name=main4',
+  HEO_INFO_CARD_ICON2: 'fab fa-tiktok',
   HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
