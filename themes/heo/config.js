f5ed9f66bc1448d53f471972a404c4026e21223b
XY159587 <111336691+XY159587@users.noreply.github.com>
2026-02-16 16:16:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -86,8 +86,8 @@ const CONFIG = {
       title_1: '红楼梦',
       img_1: '/images/heo/hongloumeng.webp',
       color_1: '#f0f0f0',
-      title_2: '霍乱时期的爱情'，
-      img_2: '/images/heo/huoluanshiqideaiqing.webp'，
+      title_2: '霍乱时期的爱情',
+      img_2: '/images/heo/huoluanshiqideaiqing.webp',
       color_2: '#f0f0f0'
     },
     {
