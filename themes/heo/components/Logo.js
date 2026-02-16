dc1468f7b460768b79f698182ccb5498696dcecb
XY159587 <111336691+XY159587@users.noreply.github.com>
2026-02-16 23:03:28 +0800

    Update Logo.js
    稍微放大头像，添加圆角

--- themes/heo/components/Logo.js
+++ themes/heo/components/Logo.js
@@ -10,16 +10,16 @@ const Logo = props => {
       <div className='flex flex-nowrap items-center cursor-pointer font-extrabold'>
         <LazyImage
           src={siteInfo?.icon}
-          width={24}
-          height={24}
+          width={32} // 放大头像
+          height={32} // 放大头像
           alt={siteConfig('AUTHOR')}
-          className='mr-4 hidden md:block'
+          className='mr-4 hidden md:block rounded-full' // 添加圆角
         />
         <div id='logo-text' className='group rounded-2xl flex-none relative'>
           <div className='logo group-hover:opacity-0 opacity-100 visible group-hover:invisible text-lg my-auto rounded dark:border-white duration-200'>
             {siteConfig('TITLE')}
           </div>
-          <div className='flex justify-center rounded-2xl group-hover:bg-indigo-600 w-full group-hover:opacity-100 opacity-0 invisible group-hover:visible absolute top-0 py-1 duration-200'>
+          <div className='flex justify-center rounded-2xl group-hover:bg-indigo-600 w-full group-hover:opacity-100 opacity-0 invisible group-hover:visible absolute top-0 p-2 text-white'>
             <Home className={'w-6 h-6 stroke-white stroke-2 '} />
           </div>
         </div>
